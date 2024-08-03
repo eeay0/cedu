69f59c9d77bd02a8f0b8a1db49598e88a1c6e373
Enes Emre AYDOGAN <eeay0@protonmail.com>
2024-08-03 07:47:57 +0300

    chapter 3

---
+++ lua/3/3-1.lua
@@ -0,0 +1,12 @@
+print(.0e12)
+-- print(.e12) no digit before the exponent
+-- print(0x) there should be digits after 0x
+print(0x1P10)
+-- print(0.0e) Incomplete
+print(0.1e1)
+print(0x12)
+-- print(0xABFG) there is no g in hexadecimal
+print(0x0.1p1)
+print(0xA)
+-- print(FFFF)
+print(0xFFFFFFFF)
