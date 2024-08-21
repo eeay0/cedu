10cc716f7e994472986ba4bbc425b137dce73efe
Enes Emre AYDOGAN <eeay0@protonmail.com>
2024-08-21 09:49:19 +0300

    chapter-4 finished

---
+++ c/modern-approach/chapter-4/programming-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1, num2, num3;
+
+    printf("Enter a three-digit number:");
+    scanf("%1d%1d%1d", &num1, &num2, &num3);
+
+    printf("The reversal is: %d%d%d", num3, num2, num1);
+}
